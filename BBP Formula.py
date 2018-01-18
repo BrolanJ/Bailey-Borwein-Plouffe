5dc5c47cdcbf50be2f1d2d38720e0185258076cb
BrolanJ <35546601+BrolanJ@users.noreply.github.com>
2018-01-17 19:06:05 -0500

    Add files via upload

---
+++ BBP Formula.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 18:54:01 2018
+
+@author: Brolan
+
+Credit: Fermat's Library
+"""
+from decimal import Decimal
+from decimal import getcontext
+
+def pi(precision):
+    getcontext().prec=precision
+    return sum(1/Decimal(16)**k * 
+        (Decimal(4)/(8*k+1) - 
+         Decimal(2)/(8*k+4) - 
+         Decimal(1)/(8*k+5) -
+         Decimal(1)/(8*k+6)) for k in range (precision))
+print(pi(10))
